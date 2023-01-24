63ffc7a85534cea4602a175663c70f830a0d4a83
HuzaifaUsmani <huzaifa.1019@gmail.com>
2023-01-24 11:30:16 +0500

    initiating DSA Concepts

---
+++ Merge.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* next;
+
+    bool insert(node);
+    bool _delete(node);
+};
+
+
+int main(){
+}
