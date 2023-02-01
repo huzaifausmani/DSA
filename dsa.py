1f4b7aa67d3a957522357de2f3121b0fae22b7c6
HuzaifaUsmani <huzaifa.1019@gmail.com>
2023-02-01 11:15:29 +0500

    encryption module for FYP

--- dsa.py
+++ dsa.py
@@ -18,10 +18,19 @@
 # # no idea what python doing here.
 
 from cryptography.fernet import Fernet
-password = "2334011019huzaifa"
+
+user = {'username': 'huzaifausmani', 'password': '2334011019', 'email': 'bitf19a007@pucit.edu.pk', 'img': 'UserImages\\2023-02-01_105927.038568White_Black_Simple_Initial_Logo.png', 'verfiedStatus': True, 'randomId': 8, 'Projects': []}
 key = Fernet(b'fcxf3NszWl5nwy0uuntnB3BsfDz1vzKgpbA2iMQb8L8=')
-encrypted = key.encrypt(password.encode())
-print(encrypted)
-decrypted = key.decrypt(encrypted).decode()
-print(decrypted)
-print(key.decrypt(b'gAAAAABj01hwU71m7wgB_fjYUUFWmAc2rzHIj0Rn1ndYquIhOPqXByW8qjomEq8o-lyRDfOpH39IFrnF8qwdLNX5O8QMHrA9ySfVFsKZ52C1DOqqwulqPO8=').decode())
+print(user)
+user.update(
+    {'username':key.encrypt(user.get('username').encode()),
+     'password':key.encrypt(user.get('password').encode()), 
+     'email':key.encrypt(user.get('email').encode())}
+    )
+print(user)
+user.update(
+    {'username':key.decrypt(user.get('username')).decode(),
+     'password':key.decrypt(user.get('password')).decode(), 
+     'email':key.decrypt(user.get('email')).decode()}
+    )
+print(user)
